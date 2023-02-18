657ef125567c479ebb08df399e1b3252c016a086
Tegar Sanova <99526526+tegarsm@users.noreply.github.com>
2023-02-18 15:01:11 +0700

    Update OverviewViewModel.kt

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.marsphotos.overview
 
 import androidx.lifecycle.LiveData
@@ -27,34 +11,33 @@ import kotlinx.coroutines.launch
 enum class MarsApiStatus { LOADING, ERROR, DONE }
 
 /**
- * The [ViewModel] that is attached to the [OverviewFragment].
- * Disini kita menyimpan seluruh data objek
+ * [ViewModel] yang dilampirkan ke [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
+    // MutableLiveData internal yang menyimpan status permintaan terbaru
     private val _status = MutableLiveData<MarsApiStatus>()
 
-    // The external immutable LiveData for the request status
+    // LiveData eksternal yang tidak dapat diubah untuk status permintaan
     val status: LiveData<MarsApiStatus> = _status
 
-    // Internally, we use a MutableLiveData, because we will be updating the List of MarsPhoto
-    // with new values
+    // Secara internal, kami menggunakan MutableLiveData, karena kami akan memperbarui Daftar MarsPhoto
+    // dengan nilai baru
     private val _photos = MutableLiveData<List<MarsPhoto>>()
 
-    // The external LiveData interface to the property is immutable, so only this class can modify
+    // Antarmuka LiveData eksternal ke properti tidak dapat diubah, jadi hanya kelas ini yang dapat memodifikasi
     val photos: LiveData<List<MarsPhoto>> = _photos
 
     /**
-     * Call getMarsPhotos() on init so we can display status immediately.
+     * Panggil getMarsPhotos() pada init agar kami dapat segera menampilkan status.
      */
     init {
         getMarsPhotos()
     }
 
     /**
-     * Gets Mars photos information from the Mars API Retrofit service and updates the
-     * [MarsPhoto] [List] [LiveData].
+     * Mendapatkan informasi foto Mars dari layanan Mars API Retrofit dan memperbarui
+     * [MarsPhoto] [Daftar] [LiveData].
      */
     private fun getMarsPhotos() {
 
