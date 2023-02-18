5b24eb64baeac49f36923e485b9af8ddb097235a
Tegar Sanova <99526526+tegarsm@users.noreply.github.com>
2023-02-18 14:58:18 +0700

    Update MarsApiService.kt

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.marsphotos.network
 
 import com.squareup.moshi.Moshi
@@ -44,17 +28,18 @@ private val retrofit = Retrofit.Builder()
  */
 interface MarsApiService {
     /**
-     * Returns a [List] of [MarsPhoto] and this method can be called from a Coroutine.
-     * The @GET annotation indicates that the "photos" endpoint will be requested with the GET
-     * HTTP method
+     * Mengembalikan [Daftar] dari [MarsPhoto] dan metode ini dapat dipanggil dari Coroutine.
+     * Anotasi @GET menunjukkan bahwa titik akhir "foto" akan diminta dengan GET
+     * Metode HTTP
      */
     @GET("photos")
     suspend fun getPhotos(): List<MarsPhoto>
 }
 
 /**
- * A public Api object that exposes the lazy-initialized Retrofit service
+ * Objek Api publik yang mengekspos layanan Retrofit yang diinisialisasi dengan malas
  */
 object MarsApi {
+    // Implementasi API untuk mengentukan interface layanan.
     val retrofitService: MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
 }
