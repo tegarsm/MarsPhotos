644f5c49337e09b6cd6f1c1ec343470eeaeea9c6
Tegar Sanova <99526526+tegarsm@users.noreply.github.com>
2023-02-18 14:59:15 +0700

    Update MarsPhoto.kt

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.marsphotos.network
 
 import com.squareup.moshi.Json
@@ -23,6 +7,6 @@ import com.squareup.moshi.Json
  * Nama properti dari kelas data ini digunakan oleh Moshi untuk mencocokkan nama nilai di JSON. */
 data class MarsPhoto(
         val id: String,
-        // used to map img_src from the JSON to imgSrcUrl in our class
+        // digunakan untuk memetakan img_src dari JSON ke imgSrcUrl di kelas kita
         @Json(name = "img_src") val imgSrcUrl: String
 )
